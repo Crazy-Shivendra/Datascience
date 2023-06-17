6534a0942db83aa73fa6cdf9583fa867602a4daf
Crazy-Shivendra <136879283+Crazy-Shivendra@users.noreply.github.com>
2023-06-17 12:24:35 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+a=int(input("enetr 1st no."))
+b=int(input("enetr 2nd no."))
+c=input("enetr operator")
+if(c=="+"):
+    print(a+b)
+if(c=="-"):
+    print(a-b)
+if(c=="/"):
+    print(a/b)
+if(c=="*"):
+    print(a*b)
+    
